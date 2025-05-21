00d71df81ab37c114e555d9747469ff0a6d1e5e4
Juvenal Latricio <jlatricio@gmail.com>
2025-05-21 16:30:58 +0100

    Adiciona todas as funcionalidades para gestão de artigos

--- src/app/features/artigo/service/artigo.service.ts
+++ src/app/features/artigo/service/artigo.service.ts
@@ -2,41 +2,40 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { Artigo } from '../interface/artigo';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtigoService {
 
-    private readonly apiUrl = '';
-  http: any;
+  private apiUrl = `${environment.apiUrl}/articles`;
 
-    constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
-    salvarArtigo(registro: Artigo) {
-      return this.httpClient.post<Artigo>(this.apiUrl, registro)
-      .pipe(
-        tap(artigo => console.log(artigo))
-      );
+  // Lista todos os artigos
+   listarArtigo(): Observable<Artigo[]> {
+     return this.httpClient.get<Artigo[]>(this.apiUrl);
+   }
+
+    // Obtém um artigo por ID
+    obterArtigoPorId(id: number): Observable<Artigo> {
+      return this.httpClient.get<Artigo>(`${this.apiUrl}/${id}`);
     }
 
-    listaArtigos() {
-      return this.httpClient.get<Artigo[]>(this.apiUrl)
-      .pipe(
-        tap(artigos => console.log(artigos))
-      );
+    // Cria um novo artigo
+    criarArtigo(artigo: Partial<Artigo>): Observable<Artigo> {
+      return this.httpClient.post<Artigo>(this.apiUrl, artigo);
     }
 
+    // Atualiza um artigo existente
+    atualizarArtigo(id: number, artigo: Partial<Artigo>): Observable<Artigo> {
+      return this.httpClient.patch<Artigo>(`${this.apiUrl}/${id}`, artigo);
+    }
+
+    // Deleta um artigo por ID
     deletarArtigo(id: number): Observable<void> {
-      // Replace the URL with the correct API endpoint
       return this.httpClient.delete<void>(`${this.apiUrl}/${id}`);
-    }
-
-    atualizarArtigo(id: number, registro: Artigo): Observable<Artigo> {
-      return this.httpClient.put<Artigo>(`${this.apiUrl}/${id}`, registro)
-      .pipe(
-      tap(artigoAtualizado => console.log(artigoAtualizado))
-      );
     }
 
 }
