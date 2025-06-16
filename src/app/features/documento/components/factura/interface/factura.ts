7b0d3ec5abc39d72fb5f8a9abf40a878f8b5105e
Juvenal Latricio <jlatricio@gmail.com>
2025-06-16 18:25:03 +0100

    Configura nota credito

--- src/app/features/documento/components/factura/interface/factura.ts
+++ src/app/features/documento/components/factura/interface/factura.ts
@@ -2,9 +2,23 @@ export interface ItemFactura {
   quantidade: number;
   artigoId: number;
   total: number;
+  categoriaId?: number;
+    precoUnitario?: number;
+  artigoSelecionado?: {
+    nome: string;
+    preco: number;
+    tipo?: string;
+    categoriaId?: number;
+  };
+
+  nomeArtigo?: string;
+  tipoArtigo?: string;
 }
 
+
 export interface factura {
+   referenciaDocumentoId?: number;
+  motivo?: string;
   tipo: string;
   numero: string;
   clienteId: number;
